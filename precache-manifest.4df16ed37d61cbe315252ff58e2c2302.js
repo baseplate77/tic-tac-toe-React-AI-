b9abec5b360b64b92c86ccf3b5f420ca537562fb
Nayan <base8087@gmail.com>
2020-12-14 15:45:51 +0530

    Updates

---
+++ precache-manifest.4df16ed37d61cbe315252ff58e2c2302.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a231bb45e66783730d5fdf15319fb942",
+    "url": "/tic-tac-toe-React-AI-/index.html"
+  },
+  {
+    "revision": "bb8ee7934009640f3cf5",
+    "url": "/tic-tac-toe-React-AI-/static/css/main.c48b2d9a.chunk.css"
+  },
+  {
+    "revision": "4c746654fb5e70653184",
+    "url": "/tic-tac-toe-React-AI-/static/js/2.88d1731b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tic-tac-toe-React-AI-/static/js/2.88d1731b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb8ee7934009640f3cf5",
+    "url": "/tic-tac-toe-React-AI-/static/js/main.f7e75a7f.chunk.js"
+  },
+  {
+    "revision": "fe10e12b45c77f049a10",
+    "url": "/tic-tac-toe-React-AI-/static/js/runtime-main.68733618.js"
+  },
+  {
+    "revision": "614756d295faab961f2f9e4296822ed1",
+    "url": "/tic-tac-toe-React-AI-/static/media/reset.614756d2.svg"
+  }
+]);
